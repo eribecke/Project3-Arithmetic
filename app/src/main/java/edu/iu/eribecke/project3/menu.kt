b9adaad56c6cc958eeaf98613f9b5ad4e360eb1a
Erika Becker <97013344+eribecke@users.noreply.github.com>
2023-09-19 21:58:02 -0400

    finished fragment two layout

--- app/src/main/java/edu/iu/eribecke/project3/menu.kt
+++ app/src/main/java/edu/iu/eribecke/project3/menu.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,31 +31,25 @@ class menu : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    class WelcomeFragment : Fragment() {
+
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            val view = inflater.inflate(R.layout.fragment_menu, container, false)
+            val startButton = view.findViewById<Button>(R.id.start)
+
+            startButton.setOnClickListener {
+                view.findNavController()
+                    .navigate(R.id.action_welcomeFragment_to_messageFragment)
+            }
+            return view
+        }
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment menu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            menu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
